df18f16f1a1292d076b2ebb4f8e3ee6aa51960cc
Philipp Kochanski <P.Kochanski@uni-duesseldorf.de>
2016-12-14 00:48:25 +0100

    Update day_5_part_1.py

--- day_5_part_1.py
+++ day_5_part_1.py
@@ -1,14 +1,11 @@
 import hashlib
 door_id = "uqwqemis"
 password = ""
-found = 0
-i = 0
+found, i = 0, 0
 
 while found < 8:
-  to_hash = door_id + str(i)
-  res = hashlib.md5(to_hash.encode("utf-8")).hexdigest()
+  res = hashlib.md5((door_id + str(i)).encode("utf-8")).hexdigest()
   if res[:5] == '00000':
-    print(to_hash, res, res[5])
     found += 1
     password += res[5]
   i += 1
